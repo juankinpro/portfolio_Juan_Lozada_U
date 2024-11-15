265e2244b39e645bf5e066df6fa330a002a09aec
juankinpro <juan.lozada696@pascualbravo.edu.co>
2024-11-15 16:03:14 -0500

    Trabajo sin completar pero con algunas bases
    este trabajo no representa ni un 10% de lo que deseo hacer aun pero no me da tiempo para terminarlo hoy 15/11
    vuelvo a trabajar en el lunes o domingo ya que estos fines de semana los trabajo todo el dia,

--- portfolio_juan/src/Screens/Home.js
+++ portfolio_juan/src/Screens/Home.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-scroll";
+import styles from "./Home.module.css";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div name="Home" className={styles.home}>
+      <div className={styles.titleContainer}>
+        <p>Home<br />
+         Pantalla principal
+      </p>
+        <p>
+        Aqui pondria lo que sea que quiera pero como no se que poner.<br/>
+        <b>Ella es fanatica de lo sensual</b>
+      </p>
+      </div>
+      <div className={styles.ctaContainer}>
+        <Link
+          to="Contact"
+          smooth
+          duration={500}
+          className={styles.callToAction}
+        >
+          Ella tiene una FOTOMIA
+        </Link>
+      </div>
+    </div>
+  );
 }
