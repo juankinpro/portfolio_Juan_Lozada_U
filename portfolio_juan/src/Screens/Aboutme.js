bedb71201bd2cd1e745e423426f074cec6afb369
juankinpro <juan.lozada696@pascualbravo.edu.co>
2024-11-22 12:13:51 -0500

    Casi terminado.
    Falta agregarle la funcionalidad a algunos botones y tengo pensado agregar un Gif al momento de presionar el botón de Home dentro de Home

--- portfolio_juan/src/Screens/Aboutme.js
+++ portfolio_juan/src/Screens/Aboutme.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styles from "../Screens/Aboutme.module.css";
+import Step from "../Components/Step";
 
 const Aboutme = () => {
+  const steps = [
+    {
+      text: " Mi nombre es Juan Lozada.",
+      id: 1,
+    },
+    {
+      text: " Me gusta mas el Back end.",
+      id: 2,
+    },
+    {
+      text: " Soy pesimo para el Front.",
+      id: 3,
+    },
+  ];
+
   return (
-    <div>Aboutme</div>
-  )
-}
+    <div name="Aboutme" className={styles.Aboutme}>
+      <h2 className={styles.title}>Quien soy</h2>
+      {steps.map((x) => (
+        <Step text={x.text} step={x.id} />
+      ))}
+    </div>
+  );
+};
 
 export default Aboutme
--- portfolio_juan/src/Screens/Services.js
+++ portfolio_juan/src/Screens/Services.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from "../Screens/Services.module.css";
 
 export const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className={styles.services}>
+      <p>
+        Puedo ayudarte a solucionar pequeños problemas de programación y bases de datos.
+      </p>
+      <img
+        className={styles.webImage}  // Asegúrate de que esta clase esté siendo usada
+        src={require("../assets/imagendeservices.jpg")}
+      />
+    </div>
+  );
+};
